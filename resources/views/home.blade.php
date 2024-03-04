d2079eddd67e56fb3ac2c4acc87c1291cf4a7ed4
LanangDepok <bagasrizkiyanto@gmail.com>
2024-03-04 21:24:53 +0700

    without db

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -102,42 +102,242 @@
         @endcan
         <h1 class="text-center mb-4">Destinasi Pilihan</h1>
         <div class="row row-cols-1 row-cols-md-3 g-4">
-            @foreach ($data as $destination)
-                <div class="col-md-4">
-                    <div class="card border-0 shadow-sm" data-aos="fade-up">
-                        <form action="/destination/{{ $destination->id }}" method="POST">
-                            @csrf
-                            <div style="height: 200px; overflow: hidden;">
-                                <img src="{{ asset('storage/' . $destination->image) }}" class="card-img-top rounded-3" alt="{{ $destination->name }}" style="object-fit: cover; width: 100%; height: 100%;">
+            {{-- @foreach ($data as $destination) --}}
+            <div class="col-md-4">
+                <div class="card border-0 shadow-sm" data-aos="fade-up">
+                    <form action="/destination//" method="POST">
+                        @csrf
+                        <div style="height: 200px; overflow: hidden;">
+                            <img src="{{ asset('/storage/1.jpg') }}" class="card-img-top rounded-3"
+                                style="object-fit: cover; width: 100%; height: 100%;">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title text-center fw-bold">Gunung Bromo</h5>
+                            <p class="card-text" style="height: 70px; overflow: hidden; text-overflow: ellipsis;">
+                                Gunung Bromo atau dalam bahasa Tengger dieja "Brama", juga disebut Kaldera Tengger, adalah
+                                sebuah gunung berapi aktif di Jawa Timur, Indonesia. Gunung ini memiliki ketinggian 2.329
+                                meter di atas permukaan laut dan berada dalam empat wilayah kabupaten, yakni Kabupaten
+                                Probolinggo, Kabupaten Pasuruan, Kabupaten Lumajang, dan Kabupaten Malang. Gunung Bromo
+                                terkenal sebagai objek wisata utama di Jawa Timur. Sebagai sebuah objek wisata, Bromo
+                                menjadi menarik karena statusnya sebagai gunung berapi yang masih aktif. Gunung Bromo
+                                termasuk dalam kawasan Taman Nasional Bromo Tengger Semeru.
+                            </p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item border-0">Lokasi:
+                                <a style="text-decoration: none" href="https://maps.app.goo.gl/ZU2yHg4sXdzaSE2N6">klik di
+                                    sini</a>
+                            </li>
+                            <!-- Add more details as needed -->
+                        </ul>
+                        <div class="card-footer bg-white border-0 text-center"> <!-- Center-align buttons -->
+                            <small class="text-muted">5 menit yang lalu</small>
+                            <div class="mt-3"> <!-- Add margin-top for spacing -->
+                                {{-- <a href="/destination/1" class="btn btn-primary btn-sm mx-1">Detail</a> --}}
+                                @can('admin')
+                                    <a href="/destination///edit" class="btn btn-warning btn-sm mx-1">Edit</a>
+                                    @method('DELETE')
+                                    <button class="btn btn-danger btn-sm mx-1" type="button"
+                                        onclick="confirmDelete('/')">Delete</button>
+                                @endcan
                             </div>
-                            <div class="card-body">
-                                <h5 class="card-title text-center fw-bold">{{ $destination->name }}</h5>
-                                <p class="card-text" style="height: 70px; overflow: hidden; text-overflow: ellipsis;">
-                                    {{ $destination->description }}
-                                </p>
-                            </div>
-                            <ul class="list-group list-group-flush">
-                                <li class="list-group-item border-0">Lokasi: </br>{{ $destination->location }}</li>
-                                <!-- Add more details as needed -->
-                            </ul>
-                            <div class="card-footer bg-white border-0 text-center"> <!-- Center-align buttons -->
-                                <small class="text-muted">{{ $destination->created_at->diffForHumans() }}</small>
-                                <div class="mt-3"> <!-- Add margin-top for spacing -->
-                                    <a href="/destination/{{ $destination->id }}" class="btn btn-primary btn-sm mx-1">Detail</a>
-                                    @can('admin')
-                                        <a href="/destination/{{ $destination->id }}/edit" class="btn btn-warning btn-sm mx-1">Edit</a>
-                                        @method('DELETE')
-                                        <button class="btn btn-danger btn-sm mx-1" type="button" onclick="confirmDelete('{{ $destination->id }}')">Delete</button>
-                                    @endcan
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+                        </div>
+                    </form>
                 </div>
-            @endforeach
+            </div>
+            <div class="col-md-4">
+                <div class="card border-0 shadow-sm" data-aos="fade-up">
+                    <form action="/destination//" method="POST">
+                        @csrf
+                        <div style="height: 200px; overflow: hidden;">
+                            <img src="{{ asset('/storage/2.jpg') }}" class="card-img-top rounded-3" alt="JaTim Park"
+                                style="object-fit: cover; width: 100%; height: 100%;">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title text-center fw-bold">JaTim Park</h5>
+                            <p class="card-text" style="height: 70px; overflow: hidden; text-overflow: ellipsis;">
+                                Jawa Timur Park adalah sebuah tempat rekreasi dan taman belajar masa kini
+                                yang menawarkan permainan, pengetahuan hingga hiburan dan menjadi salah satu icon
+                                wisata Jawa Timur yang terdapat di Kota Batu.
+                            </p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item border-0">Lokasi: <a
+                                    href="https://maps.app.goo.gl/oGLi1JfxHBNCHofLA">klik di sini</a></li>
+                            <!-- Add more details as needed -->
+                        </ul>
+                        <div class="card-footer bg-white border-0 text-center"> <!-- Center-align buttons -->
+                            <small class="text-muted">5 menit yang lalu</small>
+                            <div class="mt-3"> <!-- Add margin-top for spacing -->
+                                {{-- <a href="/destination/1" class="btn btn-primary btn-sm mx-1">Detail</a> --}}
+                                @can('admin')
+                                    <a href="/destination///edit" class="btn btn-warning btn-sm mx-1">Edit</a>
+                                    @method('DELETE')
+                                    <button class="btn btn-danger btn-sm mx-1" type="button"
+                                        onclick="confirmDelete('/')">Delete</button>
+                                @endcan
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card border-0 shadow-sm" data-aos="fade-up">
+                    <form action="/destination//" method="POST">
+                        @csrf
+                        <div style="height: 200px; overflow: hidden;">
+                            <img src="{{ asset('/storage/3.jpg') }}" class="card-img-top rounded-3"
+                                style="object-fit: cover; width: 100%; height: 100%;">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title text-center fw-bold">Kampung Warna Warni Jodipan di Malang</h5>
+                            <p class="card-text" style="height: 70px; overflow: hidden; text-overflow: ellipsis;">
+                                Kampung Warna Warni Jodipan menjadi salah satu tempat wisata unik dan favorit di Kota
+                                Malang. Rumah-rumah di Kampung Jodipan ini di cat warna-warni. Terlihat indah dan menjunjung
+                                tinggi nilai seni.
+                            </p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item border-0">Lokasi: <a
+                                    href="https://maps.app.goo.gl/tbbhenLS9aY9ZZGX9">klik di sini</a></li>
+                            <!-- Add more details as needed -->
+                        </ul>
+                        <div class="card-footer bg-white border-0 text-center"> <!-- Center-align buttons -->
+                            <small class="text-muted">5 menit yang lalu</small>
+                            <div class="mt-3"> <!-- Add margin-top for spacing -->
+                                {{-- <a href="/destination/1" class="btn btn-primary btn-sm mx-1">Detail</a> --}}
+                                @can('admin')
+                                    <a href="/destination///edit" class="btn btn-warning btn-sm mx-1">Edit</a>
+                                    @method('DELETE')
+                                    <button class="btn btn-danger btn-sm mx-1" type="button"
+                                        onclick="confirmDelete('/')">Delete</button>
+                                @endcan
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card border-0 shadow-sm" data-aos="fade-up">
+                    <form action="/destination//" method="POST">
+                        @csrf
+                        <div style="height: 200px; overflow: hidden;">
+                            <img src="{{ asset('/storage/4.jpg') }}" class="card-img-top rounded-3"
+                                style="object-fit: cover; width: 100%; height: 100%;">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title text-center fw-bold">Pantai Pasir Putih</h5>
+                            <p class="card-text" style="height: 70px; overflow: hidden; text-overflow: ellipsis;">
+                                Pantai Karanggongso atau Pantai Pasir Putih Trenggalek adalah sebuah pantai pasir putih
+                                indah yang berada di Kecamatan Watulimo, Kabupaten Trenggalek, Jawa Timur. Pantai
+                                Karanggongso terletak sekitar 1 km dari pantai Prigi, terkenal dengan pasir putihnya. Ombak
+                                di pantai ini relatif tenang, sehingga sangat cocok untuk berenang dan mandi. Di sepanjang
+                                pantai tumbuh rimbun pohon-pohon yang menambah sejuk udara pantai di siang hari.Pantai
+                                Karanggongso atau Pantai Pasir Putih Trenggalek adalah sebuah pantai pasir putih indah yang
+                                berada di Kecamatan Watulimo, Kabupaten Trenggalek, Jawa Timur. Pantai Karanggongso terletak
+                                sekitar 1 km dari pantai Prigi, terkenal dengan pasir putihnya. Ombak di pantai ini relatif
+                                tenang, sehingga sangat cocok untuk berenang dan mandi. Di sepanjang pantai tumbuh rimbun
+                                pohon-pohon yang menambah sejuk udara pantai di siang hari.
+                            </p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item border-0">Lokasi: <a
+                                    href="https://maps.app.goo.gl/XHKkBDiowG9JKZYu8">klik di sini</a></li>
+                            <!-- Add more details as needed -->
+                        </ul>
+                        <div class="card-footer bg-white border-0 text-center"> <!-- Center-align buttons -->
+                            <small class="text-muted">5 menit yang lalu</small>
+                            <div class="mt-3"> <!-- Add margin-top for spacing -->
+                                {{-- <a href="/destination/1" class="btn btn-primary btn-sm mx-1">Detail</a> --}}
+                                @can('admin')
+                                    <a href="/destination///edit" class="btn btn-warning btn-sm mx-1">Edit</a>
+                                    @method('DELETE')
+                                    <button class="btn btn-danger btn-sm mx-1" type="button"
+                                        onclick="confirmDelete('/')">Delete</button>
+                                @endcan
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card border-0 shadow-sm" data-aos="fade-up">
+                    <form action="/destination//" method="POST">
+                        @csrf
+                        <div style="height: 200px; overflow: hidden;">
+                            <img src="{{ asset('/storage/5.jpg') }}" class="card-img-top rounded-3"
+                                style="object-fit: cover; width: 100%; height: 100%;">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title text-center fw-bold">Gunung Semeru</h5>
+                            <p class="card-text" style="height: 70px; overflow: hidden; text-overflow: ellipsis;">
+                                Gunung Semeru atau Gunung Meru adalah sebuah gunung berapi kerucut di Jawa Timur, Indonesia.
+                                Gunung Semeru merupakan gunung tertinggi di Pulau Jawa, dengan puncaknya Mahameru, 3.676
+                                meter dari permukaan laut. Gunung ini terbentuk akibat subduksi Lempeng Indo-Australia
+                                kebawah Lempeng Eurasia.
+                            </p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item border-0">Lokasi: <a
+                                    href="https://maps.app.goo.gl/QoMNtD8vPgMF8hyv7">klik di sini</a></li>
+                            <!-- Add more details as needed -->
+                        </ul>
+                        <div class="card-footer bg-white border-0 text-center"> <!-- Center-align buttons -->
+                            <small class="text-muted">5 menit yang lalu</small>
+                            <div class="mt-3"> <!-- Add margin-top for spacing -->
+                                {{-- <a href="/destination/1" class="btn btn-primary btn-sm mx-1">Detail</a> --}}
+                                @can('admin')
+                                    <a href="/destination///edit" class="btn btn-warning btn-sm mx-1">Edit</a>
+                                    @method('DELETE')
+                                    <button class="btn btn-danger btn-sm mx-1" type="button"
+                                        onclick="confirmDelete('/')">Delete</button>
+                                @endcan
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card border-0 shadow-sm" data-aos="fade-up">
+                    <form action="/destination//" method="POST">
+                        @csrf
+                        <div style="height: 200px; overflow: hidden;">
+                            <img src="{{ asset('/storage/1.jpg') }}" class="card-img-top rounded-3"
+                                style="object-fit: cover; width: 100%; height: 100%;">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title text-center fw-bold">Telaga Sarangan</h5>
+                            <p class="card-text" style="height: 70px; overflow: hidden; text-overflow: ellipsis;">
+                                Telaga Sarangan, juga dikenal sebagai Telaga Pasir adalah telaga alami yang berada di
+                                ketinggian 1.200 meter di atas permukaan laut dan terletak di lereng Gunung Lawu, Kecamatan
+                                Plaosan, Kabupaten Magetan, Jawa Timur. Telaga ini berjarak sekitar 16 kilometer arah
+                                barat Kota Magetan.
+                            </p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item border-0">Lokasi: <a
+                                    href="https://maps.app.goo.gl/QDC6ah2yKzzVUPtb9">klik di sini</a></li>
+                            <!-- Add more details as needed -->
+                        </ul>
+                        <div class="card-footer bg-white border-0 text-center"> <!-- Center-align buttons -->
+                            <small class="text-muted">5 menit yang lalu</small>
+                            <div class="mt-3"> <!-- Add margin-top for spacing -->
+                                {{-- <a href="/destination/1" class="btn btn-primary btn-sm mx-1">Detail</a> --}}
+                                @can('admin')
+                                    <a href="/destination///edit" class="btn btn-warning btn-sm mx-1">Edit</a>
+                                    @method('DELETE')
+                                    <button class="btn btn-danger btn-sm mx-1" type="button"
+                                        onclick="confirmDelete('/')">Delete</button>
+                                @endcan
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            {{-- @endforeach --}}
         </div>
     </section>
-</br>
+    </br>
     <!-- Call to Action-->
     <section class="call-to-action text-white text-center" id="signup">
         <div class="container position-relative">
